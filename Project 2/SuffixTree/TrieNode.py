c1f85742b5a182b9514d0e669551d2eb3cb14f3d
hermetico <yojmarinv@gmail.com>
2016-04-28 12:31:13 +0200

    trienode

--- Project 2/SuffixTree/TrieNode.py
+++ Project 2/SuffixTree/TrieNode.py
@@ -1,7 +1,8 @@
 class TrieNode(object):
     """A suffix tree node representation"""
     # we use slots to optimize the space consumption, adding each value we are going to use
-    __slots__ = ['first_index', 'last_index', 'child', 'sibling', 'construction_number', 'dfs_number', 'marked']
+    __slots__ = ['first_index', 'last_index', 'child', 'sibling', 'construction_number',
+                 'dfs_number', 'marked', 'interval_start', 'interval_end']
 
     def __init__(self, first_index=None, last_index=None, child=None, sibling=None,
                  construction_number=None, dfs_number=None, interval_s=None, interval_e=None):
