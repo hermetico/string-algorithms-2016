832832104591b34e1125143f07e71d1ad3b44adf
Martin Bjerrum Henriksen <20105951@post.au.dk>
2016-05-02 13:43:07 +0200

    Merge branch 'master' of C:\Users\ballololz\PycharmProjects\string-algorithms-2016 with conflicts.

--- Project 2/tools/fibonacci_strings.py
+++ Project 2/tools/fibonacci_strings.py
@@ -1,45 +1,34 @@
 
 class FibonacciStrings(object):
-    def __init__(self, first='b', second='a', length=float('inf')):
+    def __init__(self, first='b', second='a'):
         self.first = first
         self.second = second
-        self.length = length
 
-    def generate(self, count=0, **kwargs):
+    def generate(self, length=0, **kwargs):
         if kwargs:
             self.__init__(**kwargs)
 
-        if count == 0:
+        if length == 0:
             return self.first
-        elif count == 1:
+        elif length == 1:
             return self.second
-        elif count == 2:
+        elif length == 2:
             return self.second + self.first
         else:
-            return self.__recursive__(self.first, self.second, count - 1)
+            return self.__recursive__(self.first, self.second, length - 1)
 
-    def generate_length(self, length=1, **kwargs):
-        if kwargs:
-            self.__init__(**kwargs)
-        return self.__recursive__(self.first, self.second, float('inf'), length)
+    def __recursive__(self, prevprev, prev, count):
 
-
-    def __recursive__(self, prevprev, prev, count, length=float('inf')):
-
-        if count == 0 or len(prev)>=length:
-            if len(prev) > length:
-                return prev[:length]
+        if count == 0:
             return prev
         else:
             new = prev + prevprev
             prevprev = prev
             prev = new
-            return self.__recursive__(prevprev, prev, count - 1, length)
+            return self.__recursive__(prevprev, prev, count - 1 )
 
 
 if __name__ == "__main__":
-    fbs = FibonacciStrings()
-    print fbs.generate(20)
-    print fbs.generate_length(50)
+    print ("hello")
 
 
