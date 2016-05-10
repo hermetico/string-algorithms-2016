b5e1c3d8ad388b382547165c191adf0ca546cdcd
Martin Bjerrum Henriksen <20105951@post.au.dk>
2016-05-10 15:28:07 +0200

    Border Search works

--- Project 3/Main.py
+++ Project 3/Main.py
@@ -3,5 +3,7 @@ from border_array import border_array as ba
 
 if __name__ == "__main__":
 
-    my_ba = ba.make_border_array("LOKI Loves to LOL")
+    my_ba = ba.make_border_array("ss$Mississippi")
     print my_ba
+
+    ba.BA_search("Mississippi", "pi")
--- Project 3/border_array/border_array.py
+++ Project 3/border_array/border_array.py
@@ -8,10 +8,24 @@ def make_border_array(x):
     for i in range (0,n-1):
         b = res[i]
         while b>0 and (x[i+1]!=x[b]):
-            b = res[b]
+            b = res[b-1]
         if x[i+1] == x[b]:
             res[i+1] = b+1
         else:
             res[i+1] = 0
     return res
+
+def BA_search(x, pattern, BA=None): #exact pattern matching with border arrays
+    if BA==None:
+        BA = make_border_array(x)
+
+    psx = pattern + "$" + x
+    print "a"
+    psx_ba = make_border_array(psx)
+    print "b"
+    m = len(pattern)
+    print psx_ba
+    for index, val in enumerate(psx_ba):
+        if val == m:
+            print index-2*m
 
