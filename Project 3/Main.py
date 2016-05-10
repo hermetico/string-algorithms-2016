1fcf339b9e8f83d656d8da0a4da6e287cf026fb0
Martin Bjerrum Henriksen <20105951@post.au.dk>
2016-05-10 16:34:00 +0200

    cleaned a little bit

--- Project 3/Main.py
+++ Project 3/Main.py
@@ -2,8 +2,4 @@ from border_array import border_array as ba
 
 
 if __name__ == "__main__":
-
-    my_ba = ba.make_border_array("ss$Mississippi")
-    print my_ba
-
     ba.BA_search("abbacbbbababacabbbba", "bbba")
