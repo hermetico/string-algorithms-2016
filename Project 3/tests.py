c558226c5ad2d9a81e48b837f993c9b82f68c7da
hermetico <yojmarinv@gmail.com>
2016-05-17 15:36:14 +0200

    tests file

---
+++ Project 3/tests.py
@@ -0,0 +1,25 @@
+from border_array import border_array as ba
+from exact_pattern_matching.exact_pattern_matching import ExactPatternMatching
+
+MIN = 10
+MAX = 2000
+STEP = 1.1
+
+methods = ['ba-search', 'kmp-search', 'naive-search']
+modes = ['worst-case', 'fibonacci-case', 'best-case']
+
+
+
+
+
+def performance(method='naive-search', mode='best-case'):
+    # TODO
+    pass
+
+def suite():
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    suite()
